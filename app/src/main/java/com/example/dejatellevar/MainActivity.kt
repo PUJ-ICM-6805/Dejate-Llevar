0e62b00c0d0f000055632af683936bfa8f92ec63
Daniel-byte628 <danigutierritos2817@gmail.com>
2023-09-01 07:20:40 -0500

    ajustes login, primerlugar y registrar

--- app/src/main/java/com/example/dejatellevar/MainActivity.kt
+++ app/src/main/java/com/example/dejatellevar/MainActivity.kt
@@ -1,29 +1,54 @@
 package com.example.dejatellevar
 
-import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
-import java.util.Calendar
+
 
 class MainActivity : AppCompatActivity() {
-    lateinit var calendario: TextView
+   // lateinit var calendario: TextView
+    lateinit var imageView: ImageView
+    lateinit var editText: EditText
+    lateinit var editTextTextPassword: EditText
+    lateinit var loginButton: Button
+    lateinit var signupButton: Button
+    lateinit var login: Button
+    lateinit var imageView2: ImageView
+    lateinit var imageView3: ImageView
+    lateinit var imageView4: ImageView
+    lateinit var textView: TextView
+    lateinit var textView2: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.categoriaslugar1)
+        setContentView(R.layout.login)
+        initComponents()
+
 
         //calendario vista
         //calendario= findViewById(R.id.editCalendario)
 
     }
+    private fun initComponents() {
+        //calendario = findViewById(R.id.editCalendario)
+        imageView = findViewById(R.id.imageView)
+        editText = findViewById(R.id.editText)
+        editTextTextPassword = findViewById(R.id.editTextTextPassword)
+        loginButton = findViewById(R.id.loginButton)
+        signupButton = findViewById(R.id.signupButton)
+        login = findViewById(R.id.login)
+        imageView2 = findViewById(R.id.imageView2)
+        imageView3 = findViewById(R.id.imageView3)
+        imageView4 = findViewById(R.id.imageView4)
+        textView = findViewById(R.id.textView)
+        textView2 = findViewById(R.id.textView2)
+    }
 
 
     //Calendario Instrucciones
+    /*
     fun abrirCalendario(view: View) {
         val cal: Calendar = Calendar.getInstance()
         val anio: Int = cal.get(Calendar.YEAR)
@@ -35,7 +60,7 @@ class MainActivity : AppCompatActivity() {
             calendario.text = fecha
         }, anio, mes, dia)
         dpd.show()
-    }
+    }*/
 
 
 }
