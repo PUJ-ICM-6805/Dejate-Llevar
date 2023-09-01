97c135891d9471a69729ed18a9880d62aec0db60
Daniel-byte628 <danigutierritos2817@gmail.com>
2023-09-01 07:43:32 -0500

    ajustes de funcionamiento

--- app/src/main/java/com/example/dejatellevar/MainActivity.kt
+++ app/src/main/java/com/example/dejatellevar/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
-        initComponents()
+       // initComponents()
 
 
         //calendario vista
